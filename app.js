18133d5799d37ba590d20567989efaa751fd1078
marcio.denadai <marcio.denadai@docente.senai.br>
2025-06-04 07:23:36 -0300

    minor changes

--- app.js
+++ app.js
@@ -110,9 +110,7 @@ app.post("/cadastro", (req, res) => {
     : console.log(JSON.stringify(req.body));
 
   const { username, password, email, celular, cpf, rg } = req.body;
-  // Colocar aqui as validações e inclusão no banco de dados do cadastro do usuário
-  // 1. Validar dados do usuário
-  // 2. saber se ele já existe no banco
+  
   const query =
     // "SELECT * FROM users WHERE email=? OR cpf=? OR rg=? OR username=?";
     "SELECT * FROM users WHERE username=?";
